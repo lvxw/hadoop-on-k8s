9ead0ffb0fd7b1d3164e557251ab27c5f38174ad
DESKTOP-4MG0BI2\lvxw <2622478542@qq.com>
2020-02-13 18:38:11 +0800

    no message

--- bin/start-hadoop-process.sh
+++ bin/start-hadoop-process.sh
@@ -4,18 +4,21 @@ source /etc/profile
 
 if [[ $PROC_TYPE == "namenode" ]]
 then
-        if [[ "`ls -A /data/hadoop/dfs/name`" = "" ]]
+    if [[ "`ls -A /data/hadoop/dfs/name`" = "" ]]
     then
         hdfs namenode -format
-        fi
-        hadoop-daemon.sh start namenode
+    fi
+    hadoop-daemon.sh start namenode
 elif [[ $PROC_TYPE == "datanode" ]]
 then
-        hadoop-daemon.sh start datanode
+    hadoop-daemon.sh start datanode
 elif [[ $PROC_TYPE == "resourcemanager" ]]
 then
-        yarn-daemon.sh start resourcemanager
+    yarn-daemon.sh start resourcemanager
 elif [[ $PROC_TYPE == "nodemanager" ]]
 then
-        yarn-daemon.sh start nodemanager
+    yarn-daemon.sh start nodemanager
+elif [[ $PROC_TYPE == "historyserver" ]]
+then
+    mr-jobhistory-daemon.sh start historyserver
 fi
