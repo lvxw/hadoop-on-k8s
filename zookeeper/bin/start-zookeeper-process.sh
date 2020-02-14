1d18e53ab6429d9f11fb2d0491a3441ba6449259
DESKTOP-4MG0BI2\lvxw <2622478542@qq.com>
2020-02-15 01:06:54 +0800

    no message

--- zookeeper/bin/start-zookeeper-process.sh
+++ zookeeper/bin/start-zookeeper-process.sh
@@ -6,4 +6,4 @@ if [[ ! -e /data/zookeeper/data/myid ]]
 then
    echo ${NODE_NUMBER} > /data/zookeeper/data/myid
 fi
-nohup zkServer.sh start > /usr/local/zookeeper/logs/zk-start.log 2>&1 &
+nohup zkServer.sh start
